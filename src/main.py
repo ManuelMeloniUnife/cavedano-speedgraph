e2ff620baed1728ec546a8defce89f8ab7b4c4b4
Manuel Meloni <manuel.meloni@edu.unife.it>
2024-08-01 15:20:02 +0200

    bounce 1

--- src/main.py
+++ src/main.py
@@ -29,17 +29,50 @@ col6 = "#c5f04d"
 # def update_data_2(file_path):
 #
 
-def open_main_window():
-    start_window.destroy()  # Chiude la finestra di avvio
-    main_window()  # Apre la finestra principale
+
+def file_from_start():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path and ('normal' == start_window.state()):
+        open_main_window(file_path)
 
 
-def main_window():
+def file_from_root():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path and ('normal' == root.state()):
+        #        update_data_1(file_path)
+        print(file_path)
+
+
+def file2_from_root():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path and ('normal' == root.state()):
+        #        update_data_2(file_path)
+        print(file_path)
+
+
+def open_main_window(path):
+    if 'normal' == start_window.state():
+        start_window.destroy()  # Chiude la finestra di avvio
+    main_window(path)  # Apre la finestra principale
+
+
+def main_window(path):
     global root, canvas, table1, table2, fig, ax, table_frame_1, table_frame_2
     root = tk.Tk()
     root.title("Team Cavedano - speedchart")
     root.minsize(1200, 700)
     root.configure(bg=col1)
+
+    menu_bar = tk.Menu(root)
+    root.config(menu=menu_bar)
+    file_menu = tk.Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(label="File", menu=file_menu)
+    file_menu.add_command(label="Cambia file", command=file_from_root)
+    file_menu.add_command(label="Sovrapponi file", command=file2_from_root)
+    clear_menu = tk.Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(label="Cancella", menu=clear_menu)
+    clear_menu.add_command(label="Pulisci grafico", command=file2_from_root)
+
     root.mainloop()
 
 
@@ -58,7 +91,6 @@ def main():
     # Mostra l'immagine
     logo_label = tk.Label(start_window, image=logo_image, bg=col1)
     logo_label.pack(pady=20)
-
     # bottone per passare al grafico
     start_button = tk.Button(start_window,
                              text="Apri file",
@@ -73,8 +105,7 @@ def main():
                              cursor='hand2',
                              font=('Sans-serif', 14),
                              borderwidth=0,
-                             command=open_main_window)
-
+                             command=file_from_start)
     start_button.pack(pady=20)
 
     start_window.mainloop()
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,30 @@
+import re
+
+
 def read_speeds_from_file(path):
     speeds = []
-    start_reading = False
+    reading = False
+
     with open(path, 'r') as file:
         for line in file:
             line = line.strip()
-            if (line == "0,00") or (line == "00,00") or (line == "00,0") or (line == '0,0'):
-                start_reading = True
-                continue
-            if start_reading:
-                speeds.append(float(line.replace(',', '.')))
-    speeds.insert(0, 00.0)
+
+            if re.match(r'^\d+,\d+$', line):
+                if line not in ["0,00", "00,00", "00,0", "0,0"]:
+                    reading = True
+                if reading:
+                    speeds.append(float(line.replace(',', '.')))
+
+    speeds.insert(0, 0.0)
+
     incremento = 1.52
     chiave_iniziale = 0.00
     dizionario = {}
+
     for value in speeds:
         dizionario[round(chiave_iniziale, 2)] = value
         chiave_iniziale += incremento
+
     return dizionario
 
 
